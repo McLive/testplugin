959779b313cf8b8ff9e1fd4bd6c0bd9d3dcd751f
McLive <mail@mclive.eu>
2014-02-09 16:16:51 +0100

    /heile hat nun eine eigene Klasse

---
+++ testplugin/src/main/java/eu/mclive/testplugin/CE_heile.java
@@ -0,0 +1,49 @@
+package eu.mclive.testplugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CE_heile implements CommandExecutor{
+	
+	private TestPlugin plugin;
+	public CE_heile(TestPlugin testplugin) {
+		plugin = testplugin;
+	}
+	
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("heile")) {
+			if (args.length == 0) {
+				if (!(sender instanceof Player)) {
+					System.out.println("Dieser Befehl ist nur für Spieler.");
+					return true;
+				}
+				Player player = (Player) sender;
+				player.setHealth(20.0);
+				player.sendMessage(ChatColor.GREEN + "Du wurdest geheilt.");
+				return true;
+			}
+			if (args.length == 1) {
+				Player ziel = plugin.getServer().getPlayer(args[0]);
+				if (ziel == null) {
+					sender.sendMessage(ChatColor.RED + "Dieser Spieler wurde nicht gefunden!");
+					return true;
+				}
+				ziel.setHealth(20.0);
+				ziel.sendMessage(ChatColor.GREEN + "Du wurdest von geheilt!");
+				return true;
+				
+			}
+			if (args.length>1) {
+				return false;
+			}
+		}
+		
+		return false;
+		
+	}
+	
+}
--- testplugin/src/main/java/eu/mclive/testplugin/TestPlugin.java
+++ testplugin/src/main/java/eu/mclive/testplugin/TestPlugin.java
@@ -1,9 +1,6 @@
 package eu.mclive.testplugin;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TestPlugin extends JavaPlugin {
@@ -11,6 +8,7 @@ public class TestPlugin extends JavaPlugin {
 @Override
 public void onEnable() {
 	System.out.println("[TestPlugin] Plugin erfolgreich geladen!");
+	this.getCommand("heile").setExecutor(new CE_heile(this)); 
 	this.getCommand("heile").setPermissionMessage(ChatColor.RED + "Dazu fehlt dir die Berechtigung!");
 }
 
@@ -19,26 +17,6 @@ public void onDisable() {
 	System.out.println("[TestPlugin] Plugin erfolgreich deaktiviert!");
 }
 
-@Override
-public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-	if (cmd.getName().equalsIgnoreCase("heile")) {
-		if (args.length == 0) {
-			if (!(sender instanceof Player)) {
-				System.out.println("Dieser Befehl ist nur für Spieler.");
-				return true;
-			}
-			Player player = (Player) sender;
-			player.setHealth(20.0);
-			player.sendMessage(ChatColor.GREEN + "Du wurdest geheilt.");
-			return true;
-		}
-		if (args.length>0) {
-			return false;
-		}
-	}
-	
-	return false;
-	
-}
+
 
 }
