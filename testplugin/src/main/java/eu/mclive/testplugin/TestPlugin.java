b180c4ada5fe15c6d641b26f1048ab1d67599dcd
McLive <mail@mclive.eu>
2014-02-09 13:13:18 +0100

    Added crap

--- testplugin/src/main/java/eu/mclive/testplugin/TestPlugin.java
+++ testplugin/src/main/java/eu/mclive/testplugin/TestPlugin.java
@@ -11,6 +11,13 @@ public void onEnable() {
 @Override
 public void onDisable() {
 	System.out.println("Plugin erfolgreich deaktiviert!");
+	System.out.println("Test");
+	System.out.println("Test2! :D");
+	System.out.println("Test3");
+	System.out.println("Test4");
+	System.out.println("Test5");
+	System.out.println("Test6");
+	System.out.println("Test7");
 }
 	
 }
