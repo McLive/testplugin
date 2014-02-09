50a8d4d2c5f153d62b5da8bb8925036b012e65f2
McLive <mail@mclive.eu>
2014-02-09 14:16:33 +0100

    Removed testmessages on plugin disable

--- testplugin/src/main/java/eu/mclive/testplugin/TestPlugin.java
+++ testplugin/src/main/java/eu/mclive/testplugin/TestPlugin.java
@@ -11,20 +11,6 @@ public void onEnable() {
 @Override
 public void onDisable() {
 	System.out.println("Plugin erfolgreich deaktiviert!");
-	System.out.println("Test");
-	System.out.println("Test2! :D");
-	System.out.println("Test3");
-	System.out.println("Test4");
-	System.out.println("Test5");
-	System.out.println("Test6");
-	System.out.println("Test7");
-	System.out.println("Test8");
-	System.out.println("Test9");
-	System.out.println("Test10");
-	System.out.println("Test11");
-	System.out.println("Test12");
-	System.out.println("Test13");
-	System.out.println("Test14");
 }
 	
 }
