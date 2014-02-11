05543c49d65f22e1e60ba5bcb5ad51d19ecd7025
McLive <mail@mclive.eu>
2014-02-11 19:04:31 +0100

    Added/fixed lots of things

--- testplugin/src/main/java/eu/mclive/testplugin/CE_heile.java
+++ testplugin/src/main/java/eu/mclive/testplugin/CE_heile.java
@@ -28,12 +28,14 @@ public class CE_heile implements CommandExecutor{
 			}
 			if (args.length == 1) {
 				Player ziel = plugin.getServer().getPlayer(args[0]);
+				Player player = (Player) sender;
 				if (ziel == null) {
 					sender.sendMessage(ChatColor.RED + "Dieser Spieler wurde nicht gefunden!");
 					return true;
 				}
 				ziel.setHealth(20.0);
-				ziel.sendMessage(ChatColor.GREEN + "Du wurdest von geheilt!");
+				ziel.sendMessage(ChatColor.GREEN + "Du wurdest von " + ChatColor.RED + player.getName() + ChatColor.GREEN + " geheilt!");
+				player.sendMessage(ChatColor.GREEN + "Du hast " + ChatColor.RED + ziel.getName() + ChatColor.GREEN + " geheilt!");
 				return true;
 				
 			}
--- testplugin/src/main/java/eu/mclive/testplugin/TestPlugin.java
+++ testplugin/src/main/java/eu/mclive/testplugin/TestPlugin.java
@@ -1,13 +1,18 @@
 package eu.mclive.testplugin;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TestPlugin extends JavaPlugin {
+public class TestPlugin extends JavaPlugin implements Listener {
 
 @Override
 public void onEnable() {
 	System.out.println("[TestPlugin] Plugin erfolgreich geladen!");
+	getServer().getPluginManager().registerEvents(this, this);
 	this.getCommand("heile").setExecutor(new CE_heile(this)); 
 	this.getCommand("heile").setPermissionMessage(ChatColor.RED + "Dazu fehlt dir die Berechtigung!");
 }
@@ -17,6 +22,20 @@ public void onDisable() {
 	System.out.println("[TestPlugin] Plugin erfolgreich deaktiviert!");
 }
 
+@EventHandler
+public void onAttack(EntityDamageByEntityEvent event) {
+	if(event.getDamager() instanceof Player) {
+		if (event.getEntity() instanceof Player) {
+			Player attacker = null;
+			Player defender = null;
+			attacker = (Player)event.getDamager();
+			defender = (Player)event.getEntity();
+		
+			attacker.sendMessage(ChatColor.GREEN + "Du hast " + ChatColor.RED + defender.getName()  + ChatColor.GREEN + " angegriffen.");
+			defender.sendMessage(ChatColor.GREEN + "Du wurdest von " + ChatColor.RED + attacker.getName() + ChatColor.GREEN + " angegriffen.");
+		}
+	}
+}
 
 
 }
